18a112d77ba44f78ba97eb7feb8efd0a8095d706
Matthew Smith <matthewalexander108@gmail.com>
2017-11-20 20:27:39 -0500

    asdf

--- server.rb
+++ server.rb
@@ -4,15 +4,13 @@ require 'digest'
 
 server = TCPServer.new('localhost', 2345)
 
-# TODO Review 'loop'
-
 loop do
 
   # Wait for a connection
   socket = server.accept
   STDERR.puts "Incoming Request"
 
-  # Read the HTTP request. W know it's finished when we see a line with nothing but \r\n
+  # Read the HTTP request. We know it's finished when we see a line with nothing but \r\n
   http_request = ""
   while (line = socket.gets) && (line != "\r\n")
     http_request += line
@@ -32,7 +30,7 @@ loop do
   response_key = Digest::SHA1.base64digest([websocket_key, "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"].join)
 
   STDERR.puts "Responding to handshake with key: #{ response_key }"
-  
+
   socket.write <<-eos
 HTTP/1.1 101 Switching Protocols
 Upgrade: websocket
